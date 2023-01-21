c7115c777b6794f5c999ae4a6f3ecc1ad9adba09
alvinscheibe <alvin.scheibe@gmail.com>
2023-01-21 14:14:25 -0300

    Create database instance

---
+++ database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func ConnectDb() {
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Fail to connect to the database! \n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Connected to the database successfully")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Running migrations")
+	//@TODO Add migrations
+
+	Database = DbInstance{Db: db}
+}
