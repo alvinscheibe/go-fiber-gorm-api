b8fd040e0aedef913958a4b47aa1456f40039305
alvinscheibe <alvin.scheibe@gmail.com>
2023-01-25 12:03:34 -0300

    Create response order

--- routes/order.go
+++ routes/order.go
@@ -1,10 +1,22 @@
 package routes
 
-import "time"
+import (
+	"github.com/alvinscheibe/go-fiber-api/models"
+	"time"
+)
 
 type Order struct {
 	ID        uint      `json:"id"`
 	User      User      `json:"user"`
 	Product   Product   `json:"product"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func CreateOrderResponse(orderModel models.Order, user User, product Product) Order {
+	return Order{
+		ID:        orderModel.ID,
+		User:      user,
+		Product:   product,
+		CreatedAt: orderModel.CreatedAt,
+	}
 }
